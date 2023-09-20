fb2436a46ad567b9b71bae161fe7fb2dd8b90ca6
Poniatowski <72635089+PoniatowskiAleksander@users.noreply.github.com>
2023-09-20 14:49:56 +0200

    finish v1

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('hello');
+console.log("")
+
+const prompt = require('prompt-sync')();
+
+const rates = {
+  1: 4.6,
+  2: 4.3,
+  3: 5.4
+}
+
+const symbols = {
+  1: 'EUR',
+  2: 'USD',
+  3: 'GBP'
+}
+
+var currency = 0;
+
+do {
+  console.log('1 - EUR, 2 - USD, 3 - GBP');
+
+  currency = parseInt(prompt("wybierz odpowiednia walute"));
+} while (![1, 2, 3].includes(currency));
+
+var number;
+
+do {
+  //console.log(
+  number = parseInt(prompt("podaj kwote w pln: "));
+  //console.log(number);
+  //console.log(!isNaN(number));
+} while (isNaN(number));
+
+
+console.log(number + ' PLN in ' + symbols[currency] + ' is ' + 
+            Math.round(number / rates[currency] * 100)/100)
+
+
+
+
+
+
+
